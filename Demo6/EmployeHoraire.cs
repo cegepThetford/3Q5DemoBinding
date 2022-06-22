b381d55a0b142d86fe7a6b3cb5560105e042e6be
Marco Guay <mguay@cegepthetford.ca>
2022-06-22 07:27:39 -0400

    Ajoutez des fichiers projet.

---
+++ Demo6/EmployeHoraire.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo6
+{
+    public class EmployeHoraire : INotifyPropertyChanged
+    {
+        private string _nom;
+        private decimal _tauxHoraire;
+        private decimal _nbHeures;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public EmployeHoraire(string nom, decimal taux)
+        {
+            _nom = nom;
+            _tauxHoraire = taux;
+            _nbHeures = 0.0m;
+        }
+
+        public void ajouterHeures(decimal nbH)
+        {
+            //_nbHeures += nbH;
+            //if (PropertyChanged != null)
+            //    PropertyChanged(this, new PropertyChangedEventArgs("NbHeures"));
+            NbHeures += nbH;
+        }
+
+        public string Nom
+        {
+            get { return _nom; }
+            set
+            {
+                _nom = value;
+                this.notifier("Nom");
+            }
+        }
+        public decimal TauxHoraire
+        {
+            get { return _tauxHoraire; }
+            set
+            {
+                _tauxHoraire = value;
+                this.notifier("TauxHoraire");
+
+            }
+        }
+        public decimal NbHeures
+        {
+            get { return _nbHeures; }
+            set
+            {
+                _nbHeures = value;
+                this.notifier("NbHeures");
+            }
+        }
+        private void notifier(string propriete)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propriete));
+        }
+        public decimal calculerSalaireBrut()
+        {
+            decimal salaire = _tauxHoraire * _nbHeures;
+            if (_nbHeures > 75.0m)
+                salaire += 0.5m * _tauxHoraire * (_nbHeures - 75.0m);
+            return salaire;
+        }
+
+        public override int GetHashCode()
+        {
+            return Nom.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is EmployeHoraire))
+                return false;
+            EmployeHoraire empl = (EmployeHoraire)obj;
+            return empl.Nom.Equals(_nom);
+        }
+    }
+
+}
---
+++ Demo6/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Demo6
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private EmployeHoraire _employe;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _employe = new EmployeHoraire("Adam Bernard", 10.00m);
+            grille.DataContext = _employe;
+        }
+        public EmployeHoraire Employe
+        {
+            get { return _employe; }
+            set
+            {
+                _employe = value;
+            }
+        }
+        private void AjouterHeures(object sender, RoutedEventArgs e)
+        {
+            Button b = sender as Button;
+            string chaine = b.Content.ToString().Split()[1];
+            int nbHeures = int.Parse(chaine);
+            _employe.ajouterHeures(nbHeures);
+        }
+        private void btnRafraichir_Click(object sender, RoutedEventArgs e)
+        {
+            // Version 1
+            tbNom.Text = _employe.Nom;
+            tbTauxHoraire.Text = _employe.TauxHoraire.ToString();
+            tbNbHeures.Text = _employe.NbHeures.ToString();
+        }
+    }
+}
